2969ea5f8f662a53cbd68bb1a060c728d842655d
Alex <awheeler@bu.edu>
2014-05-27 14:21:52 -0400

    refactors for two parameters to set keys, adds README

--- background.js
+++ background.js
@@ -1,15 +1,18 @@
-$( document ).keydown(function(event){
-	if (event.keyCode == 76) {
-	links = document.getElementsByTagName('a');
-	for (i=0; i < links.length; i++) {
-			if (links[i].innerHTML == "Sign out") {
-				links[i].click();
+var l = 76;
+var ctrl = 'event.ctrlKey'
+
+setSignOutHotKeys(l,ctrl)
+
+function setSignOutHotKeys(key1, key2) {
+	$( document ).keydown(function(event){
+		if (event.keyCode == key1 && key2) {
+		links = document.getElementsByTagName('a');
+		for (i=0; i < links.length; i++) {
+				if (links[i].innerHTML == "Sign out") {
+					links[i].click();
+				}
 			}
 		}
-	}
-});
-
-
-
-		
+	});
+}		
 		
